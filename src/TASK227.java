d9bfcfc46963a9a027418dfbded89ac7aaa1673d
Manaks <manaksaa@gmail.com>
2021-08-09 17:23:38 +0300

    add new comment

---
+++ src/TASK227.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TASK227 {
+    public static void main(String[] agrs) {
+        Scanner v = new Scanner(System.in);
+        System.out.println("Введите размерность матрицы");
+        int n = v.nextInt();// Вввод размерности матрицы
+        double[][] X = new double[n][n];
+        System.out.println("Матрица:");
+        int sum=0;
+            for (int i = 0; i < X.length; i++) {
+                for (int j = 0; j < X[i].length; j++) {
+                    X[i][j]=Math.sin((Math.pow(i, 2)-Math.pow(j, 2))/n);
+                    System.out.print(X[i][j] + " ");
+                    if (X[i][j] > 0) {
+                        sum=sum+1;
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("Количество положительныж элементов составляет: " + sum);
+    }
+}
